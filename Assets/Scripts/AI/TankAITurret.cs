f38aafa1a24385fb2d990166213539e836b8d6c5
Jack <jacopo.derosa00@gmail.com>
2022-05-30 07:36:11 +0200

    Added Turret Rot

--- Assets/Scripts/AI/TankAITurret.cs
+++ Assets/Scripts/AI/TankAITurret.cs
@@ -6,4 +6,30 @@ public class TankAITurret : MonoBehaviour
 {
     [SerializeField] private Transform  _turretPointer, _cannonPointer;
     [SerializeField] private Transform _target;
+    [SerializeField] private float _minElevation, _maxElevation;
+
+
+    private void Update()
+    {
+        RotateTurret();
+        RotateCannon();
+    }
+
+    private void RotateTurret()
+    {
+        Vector3 localAimPoint = transform.InverseTransformPoint(_target.position);
+        localAimPoint.y = 0;
+        Quaternion look = Quaternion.LookRotation(localAimPoint);
+        _turretPointer.localRotation = look;
+    }
+    private void RotateCannon()
+    {
+        Vector3 localAimPoint = transform.InverseTransformPoint(_target.position);
+        localAimPoint.x = 0;
+        localAimPoint.z = 0;
+
+
+        Quaternion look = Quaternion.LookRotation(localAimPoint);
+        _cannonPointer.localRotation = look;
+    }
 }
